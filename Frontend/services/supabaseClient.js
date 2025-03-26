3a6f2f8afb49b3abc30efc9c7636436938c9828d
Genbuos <Swiftypython18@gmail.com>
2025-03-26 18:37:39 -0400

    reminder for 7's database

--- Frontend/services/supabaseClient.js
+++ Frontend/services/supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseAnonKey = process.env.SUPABASE_KEY
+
+// do not forget to create 7's database. also we need to think about how we are tracking the vehicles .
+// const supabaseUrl = process.env.SUPABASE_URL
+// const supabaseAnonKey = process.env.SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)

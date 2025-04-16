409232ce700534245c867a9b9a9615f5f3f74200
genbuos <swiftypython18@gmail.com>
2025-04-16 18:02:26 -0400

    stop tracking the backend config

--- Frontend/services/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import {execSync} from 'child_process'
-// need to put the url and key in keychain
-const supabaseUrl = execSync('security find-generic-password -a SUPABASE_URL -s supabase -w', { encoding: 'utf8' }).trim()
-const supabaseAnonKey = execSync('security find-generic-password -a SUPABASE_ANON_KEY -s supabase -w', { encoding: 'utf8' }).trim()
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)

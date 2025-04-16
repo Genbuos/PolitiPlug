8ba907f5fc2cc30ad7480cc945d1ce29a6e8bc06
genbuos <swiftypython18@gmail.com>
2025-04-16 17:58:26 -0400

    Backend update

--- Frontend/services/supabaseClient.js
+++ Frontend/services/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseAnonKey = process.env.SUPABASE_KEY
+import {execSync} from 'child_process'
+// need to put the url and key in keychain
+const supabaseUrl = execSync('security find-generic-password -a SUPABASE_URL -s supabase -w', { encoding: 'utf8' }).trim()
+const supabaseAnonKey = execSync('security find-generic-password -a SUPABASE_ANON_KEY -s supabase -w', { encoding: 'utf8' }).trim()
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
